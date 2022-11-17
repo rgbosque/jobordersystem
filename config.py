332ab2a759b5bc0956e89e9d4592f44c0e84f86e
Rodel Bosque <rgbosque@gmail.com>
2022-11-17 16:38:00 +0800

    install sqlalchemy , migrate and create User table

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os, secrets
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(16)
     COMPANY_NAME = "RHODECO RUBBER PROCESSING SERVICES, INC."
+    SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(16)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(BASE_DIR, 'jo.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
