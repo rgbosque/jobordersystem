92e01952893c63e8fb25a848587a231f745ff415
Rodel Bosque <rgbosque@gmail.com>
2022-11-17 09:51:50 +0800

    refactor html templates

--- app/routes.py
+++ app/routes.py
@@ -3,10 +3,12 @@ from flask import flash, redirect, render_template, url_for
 from app import app
 from app.forms import LoginForm
 
+company_name = app.config['COMPANY_NAME']
+
 
 @app.route('/')
-def home():
-    return render_template("index.html", title="Home")
+def home():    
+    return render_template("index.html", title="Home", company_name=company_name)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -14,12 +16,12 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me = {}'.format(form.username.data, form.remember_me.data))
         return redirect(url_for('dashboard'))
-    return render_template("login.html", title="Login", form=form)
+    return render_template("login.html", title="Login", form=form, company_name=company_name)
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template("dashboard.html", title="Dashboard")
+    return render_template("dashboard.html", title="Dashboard", company_name=company_name)
 
 @app.route('/aboutus')
 def about_us():
-    return render_template("about_us.html", title="About Us")
+    return render_template("about_us.html", title="About Us", company_name=company_name)
--- config.py
+++ config.py
@@ -2,3 +2,4 @@ import os, secrets
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(16)
+    COMPANY_NAME = "RHODECO RUBBER PROCESSING SERVICES, INC."
